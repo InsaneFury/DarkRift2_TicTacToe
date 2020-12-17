0eefe96c7151b9396bf9631f27537ef6baa16b52
Ivan Castellano <justdarsan@gmail.com>
2020-12-17 11:50:43 -0300

    Added game board

---
+++ Assets/Game/Scripts/Match/BoardControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject slateSample;
+
+    void Start()
+    {
+        Vector3[] positions = new Vector3[]
+        {
+        new Vector3(-1.0f,1.0f),
+        new Vector3(0f,1.0f),
+        new Vector3(1.0f,1.0f),
+
+        new Vector3(-1.0f,0.0f),
+        new Vector3(0f,0.0f),
+        new Vector3(1.0f,0.0f),
+
+        new Vector3(-1.0f,-1.0f),
+        new Vector3(0f,-1.0f),
+        new Vector3(1.0f,-1.0f),
+        };
+
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject slate = Instantiate(slateSample);
+            slate.transform.parent = gameObject.transform;
+            slate.transform.position = positions[i];
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
